54c25593b4a0a64cf8a1ee6cda49bdd475b1662b
dcccrypto <dcccrypto@users.noreply.github.com>
2026-03-30 08:40:25 +0100

    chore(sdk): rebuild dist for PERC-8256 (stale dist fix)

--- packages/core/dist/index.js
+++ packages/core/dist/index.js
@@ -1107,6 +1107,39 @@ var V2_ENGINE_NET_LP_POS_OFF = 504;
 var V2_ENGINE_LP_SUM_ABS_OFF = 520;
 var V2_ENGINE_LP_MAX_ABS_OFF = 536;
 var V2_ENGINE_LP_MAX_ABS_SWEEP_OFF = 552;
+var V1M_ENGINE_OFF = 640;
+var V1M_CONFIG_LEN = 536;
+var V1M_ACCOUNT_SIZE = 248;
+var V1M_ENGINE_PARAMS_OFF = 72;
+var V1M_PARAMS_SIZE = 336;
+var V1M_ENGINE_CURRENT_SLOT_OFF = 408;
+var V1M_ENGINE_FUNDING_INDEX_OFF = 416;
+var V1M_ENGINE_LAST_FUNDING_SLOT_OFF = 432;
+var V1M_ENGINE_FUNDING_RATE_BPS_OFF = 440;
+var V1M_ENGINE_MARK_PRICE_OFF = 448;
+var V1M_ENGINE_LAST_CRANK_SLOT_OFF = 472;
+var V1M_ENGINE_MAX_CRANK_STALENESS_OFF = 480;
+var V1M_ENGINE_TOTAL_OI_OFF = 488;
+var V1M_ENGINE_LONG_OI_OFF = 504;
+var V1M_ENGINE_SHORT_OI_OFF = 520;
+var V1M_ENGINE_C_TOT_OFF = 536;
+var V1M_ENGINE_PNL_POS_TOT_OFF = 552;
+var V1M_ENGINE_LIQ_CURSOR_OFF = 568;
+var V1M_ENGINE_GC_CURSOR_OFF = 570;
+var V1M_ENGINE_LAST_SWEEP_START_OFF = 576;
+var V1M_ENGINE_LAST_SWEEP_COMPLETE_OFF = 584;
+var V1M_ENGINE_CRANK_CURSOR_OFF = 592;
+var V1M_ENGINE_SWEEP_START_IDX_OFF = 594;
+var V1M_ENGINE_LIFETIME_LIQUIDATIONS_OFF = 600;
+var V1M_ENGINE_LIFETIME_FORCE_CLOSES_OFF = 608;
+var V1M_ENGINE_NET_LP_POS_OFF = 616;
+var V1M_ENGINE_LP_SUM_ABS_OFF = 632;
+var V1M_ENGINE_LP_MAX_ABS_OFF = 648;
+var V1M_ENGINE_LP_MAX_ABS_SWEEP_OFF = 664;
+var V1M_ENGINE_EMERGENCY_OI_MODE_OFF = 680;
+var V1M_ENGINE_EMERGENCY_START_SLOT_OFF = 688;
+var V1M_ENGINE_LAST_BREAKER_SLOT_OFF = 696;
+var V1M_ENGINE_BITMAP_OFF = 726;
 var ENGINE_OFF = V1_ENGINE_OFF;
 var ENGINE_MARK_PRICE_OFF = V1_ENGINE_MARK_PRICE_OFF;
 function computeSlabSize(engineOff, bitmapOff, accountSize, maxAccounts, postBitmap = 18) {
@@ -1123,6 +1156,7 @@ var V1_SIZES = /* @__PURE__ */ new Map();
 var V1_SIZES_LEGACY = /* @__PURE__ */ new Map();
 var V1D_SIZES = /* @__PURE__ */ new Map();
 var V2_SIZES = /* @__PURE__ */ new Map();
+var V1M_SIZES = /* @__PURE__ */ new Map();
 var V1D_SIZES_LEGACY = /* @__PURE__ */ new Map();
 for (const n of TIERS) {
   V0_SIZES.set(computeSlabSize(V0_ENGINE_OFF, V0_ENGINE_BITMAP_OFF, V0_ACCOUNT_SIZE, n), n);
@@ -1131,11 +1165,17 @@ for (const n of TIERS) {
   V1D_SIZES.set(computeSlabSize(V1D_ENGINE_OFF, V1D_ENGINE_BITMAP_OFF, V1D_ACCOUNT_SIZE, n, 2), n);
   V1D_SIZES_LEGACY.set(computeSlabSize(V1D_ENGINE_OFF, V1D_ENGINE_BITMAP_OFF, V1D_ACCOUNT_SIZE, n, 18), n);
   V2_SIZES.set(computeSlabSize(V2_ENGINE_OFF, V2_ENGINE_BITMAP_OFF, V2_ACCOUNT_SIZE, n, 18), n);
+  V1M_SIZES.set(computeSlabSize(V1M_ENGINE_OFF, V1M_ENGINE_BITMAP_OFF, V1M_ACCOUNT_SIZE, n, 18), n);
 }
 var SLAB_TIERS_V2 = {
   small: { maxAccounts: 256, dataSize: 65088, label: "Small", description: "256 slots (V2 BPF intermediate)" },
   large: { maxAccounts: 4096, dataSize: 1025568, label: "Large", description: "4,096 slots (V2 BPF intermediate)" }
 };
+var SLAB_TIERS_V1M = {};
+for (const [label, n] of [["Micro", 64], ["Small", 256], ["Medium", 1024], ["Large", 4096]]) {
+  const size = computeSlabSize(V1M_ENGINE_OFF, V1M_ENGINE_BITMAP_OFF, V1M_ACCOUNT_SIZE, n, 18);
+  SLAB_TIERS_V1M[label.toLowerCase()] = { maxAccounts: n, dataSize: size, label, description: `${n} slots (V1M mainnet)` };
+}
 function buildLayout(version, maxAccounts, engineOffOverride) {
   const isV0 = version === 0;
   const engineOff = engineOffOverride ?? (isV0 ? V0_ENGINE_OFF : V1_ENGINE_OFF);
@@ -1327,7 +1367,67 @@ function buildLayoutV2(maxAccounts) {
     engineInsuranceIsolationBpsOff: 64
   };
 }
+function buildLayoutV1M(maxAccounts) {
+  const engineOff = V1M_ENGINE_OFF;
+  const bitmapOff = V1M_ENGINE_BITMAP_OFF;
+  const accountSize = V1M_ACCOUNT_SIZE;
+  const bitmapWords = Math.ceil(maxAccounts / 64);
+  const bitmapBytes = bitmapWords * 8;
+  const postBitmap = 18;
+  const nextFreeBytes = maxAccounts * 2;
+  const preAccountsLen = bitmapOff + bitmapBytes + postBitmap + nextFreeBytes;
+  const accountsOffRel = Math.ceil(preAccountsLen / 8) * 8;
+  return {
+    version: 1,
+    headerLen: V1_HEADER_LEN,
+    configOffset: V1_HEADER_LEN,
+    configLen: V1M_CONFIG_LEN,
+    reservedOff: V1_RESERVED_OFF,
+    engineOff,
+    accountSize,
+    maxAccounts,
+    bitmapWords,
+    accountsOff: engineOff + accountsOffRel,
+    engineInsuranceOff: 16,
+    engineParamsOff: V1M_ENGINE_PARAMS_OFF,
+    paramsSize: V1M_PARAMS_SIZE,
+    engineCurrentSlotOff: V1M_ENGINE_CURRENT_SLOT_OFF,
+    engineFundingIndexOff: V1M_ENGINE_FUNDING_INDEX_OFF,
+    engineLastFundingSlotOff: V1M_ENGINE_LAST_FUNDING_SLOT_OFF,
+    engineFundingRateBpsOff: V1M_ENGINE_FUNDING_RATE_BPS_OFF,
+    engineMarkPriceOff: V1M_ENGINE_MARK_PRICE_OFF,
+    engineLastCrankSlotOff: V1M_ENGINE_LAST_CRANK_SLOT_OFF,
+    engineMaxCrankStalenessOff: V1M_ENGINE_MAX_CRANK_STALENESS_OFF,
+    engineTotalOiOff: V1M_ENGINE_TOTAL_OI_OFF,
+    engineLongOiOff: V1M_ENGINE_LONG_OI_OFF,
+    engineShortOiOff: V1M_ENGINE_SHORT_OI_OFF,
+    engineCTotOff: V1M_ENGINE_C_TOT_OFF,
+    enginePnlPosTotOff: V1M_ENGINE_PNL_POS_TOT_OFF,
+    engineLiqCursorOff: V1M_ENGINE_LIQ_CURSOR_OFF,
+    engineGcCursorOff: V1M_ENGINE_GC_CURSOR_OFF,
+    engineLastSweepStartOff: V1M_ENGINE_LAST_SWEEP_START_OFF,
+    engineLastSweepCompleteOff: V1M_ENGINE_LAST_SWEEP_COMPLETE_OFF,
+    engineCrankCursorOff: V1M_ENGINE_CRANK_CURSOR_OFF,
+    engineSweepStartIdxOff: V1M_ENGINE_SWEEP_START_IDX_OFF,
+    engineLifetimeLiquidationsOff: V1M_ENGINE_LIFETIME_LIQUIDATIONS_OFF,
+    engineLifetimeForceClosesOff: V1M_ENGINE_LIFETIME_FORCE_CLOSES_OFF,
+    engineNetLpPosOff: V1M_ENGINE_NET_LP_POS_OFF,
+    engineLpSumAbsOff: V1M_ENGINE_LP_SUM_ABS_OFF,
+    engineLpMaxAbsOff: V1M_ENGINE_LP_MAX_ABS_OFF,
+    engineLpMaxAbsSweepOff: V1M_ENGINE_LP_MAX_ABS_SWEEP_OFF,
+    engineEmergencyOiModeOff: V1M_ENGINE_EMERGENCY_OI_MODE_OFF,
+    engineEmergencyStartSlotOff: V1M_ENGINE_EMERGENCY_START_SLOT_OFF,
+    engineLastBreakerSlotOff: V1M_ENGINE_LAST_BREAKER_SLOT_OFF,
+    engineBitmapOff: V1M_ENGINE_BITMAP_OFF,
+    acctOwnerOff: ACCT_OWNER_OFF,
+    hasInsuranceIsolation: true,
+    engineInsuranceIsolatedOff: 48,
+    engineInsuranceIsolationBpsOff: 64
+  };
+}
 function detectSlabLayout(dataLen, data) {
+  const v1mn = V1M_SIZES.get(dataLen);
+  if (v1mn !== void 0) return buildLayoutV1M(v1mn);
   const v0n = V0_SIZES.get(dataLen);
   if (v0n !== void 0) return buildLayout(0, v0n);
   const v1dn = V1D_SIZES.get(dataLen);
@@ -1890,7 +1990,8 @@ var ALL_SLAB_SIZES = [
   ...Object.values(SLAB_TIERS).map((t) => t.dataSize),
   ...Object.values(SLAB_TIERS_V0).map((t) => t.dataSize),
   ...Object.values(SLAB_TIERS_V1D).map((t) => t.dataSize),
-  ...Object.values(SLAB_TIERS_V1D_LEGACY).map((t) => t.dataSize)
+  ...Object.values(SLAB_TIERS_V1D_LEGACY).map((t) => t.dataSize),
+  ...Object.values(SLAB_TIERS_V1M).map((t) => t.dataSize)
 ];
 var SLAB_DATA_SIZE = SLAB_TIERS.large.dataSize;
 var HEADER_SLICE_LENGTH = 1940;
@@ -2078,7 +2179,8 @@ async function discoverMarkets(connection, programId, options = {}) {
     ...Object.values(SLAB_TIERS_V0),
     ...Object.values(SLAB_TIERS_V1D),
     ...Object.values(SLAB_TIERS_V1D_LEGACY),
-    ...Object.values(SLAB_TIERS_V2)
+    ...Object.values(SLAB_TIERS_V2),
+    ...Object.values(SLAB_TIERS_V1M)
   ];
   let rawAccounts = [];
   async function fetchTierWithRetry(tier) {
@@ -3432,6 +3534,7 @@ export {
   SLAB_TIERS_V1,
   SLAB_TIERS_V1D,
   SLAB_TIERS_V1D_LEGACY,
+  SLAB_TIERS_V1M,
   SLAB_TIERS_V2,
   STAKE_IX,
   STAKE_POOL_SIZE,
